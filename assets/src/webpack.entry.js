8e1dca9d957f805086710f93447102389c83257a
Muneef Hameed <hey@muneef.in>
2017-12-16 15:37:22 +0530

    desing updates

--- assets/src/webpack.entry.js
+++ assets/src/webpack.entry.js
@@ -1,7 +1,6 @@
 import './main.css';
 
+/** Quick Hack - Move to Webpack Configs? */
 require('../images/facebook.svg');
 require('../images/github.svg');
 require('../images/linkedin.svg');
-
-console.log('it works');
