ecbee2f1f05a846fd9c004decc3da5c8b7569c2b
Muneef Hameed <hey@muneef.in>
2017-12-17 17:17:27 +0530

    added placeholders for projects, talks & blogs

--- assets/src/webpack.entry.js
+++ assets/src/webpack.entry.js
@@ -6,3 +6,6 @@ require('../images/github.svg');
 require('../images/linkedin.svg');
 require('../images/twitter.svg');
 require('../images/medium.svg');
+require('../images/talk.svg');
+require('../images/project.svg');
+require('../images/blog.svg');
